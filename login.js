d9aa5e70563ac094725bfdcbc7a82496dddfaa93
wallyssonstudiodv <wallyssonsd1@gmail.com>
2025-08-15 20:08:08 -0300

    Criar login.js

---
+++ login.js
@@ -0,0 +1,31 @@
+const { default: makeWASocket, useSingleFileAuthState, fetchLatestBaileysVersion, DisconnectReason } = require("@adiwajshing/baileys");
+const qrcode = require('qrcode-terminal');
+
+const SESSION_FILE = './session.json';
+const { state, saveState } = useSingleFileAuthState(SESSION_FILE);
+
+async function start() {
+    const { version } = await fetchLatestBaileysVersion();
+    const sock = makeWASocket({ auth: state, version });
+
+    sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect, qr } = update;
+        if(qr){
+            qrcode.generate(qr, {small:true});
+            console.log("Escaneie o QR Code acima com seu WhatsApp");
+        }
+        if(connection==='close'){
+            if((lastDisconnect.error)?.output?.statusCode !== 401){
+                start();
+            } else {
+                console.log('Sessão desconectada');
+            }
+        } else if(connection==='open'){
+            console.log('Conectado com sucesso!');
+        }
+    });
+
+    sock.ev.on('creds.update', saveState);
+}
+
+start();
